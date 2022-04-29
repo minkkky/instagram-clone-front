c3d93221d5295c40a4d49056b4340a204d271286
m_ink <mp98dl4@gmail.com>
2022-04-29 12:57:43 +0900

    이미지 캐러셀 기능 추가 / 댓글란 아이콘 추가, 인풋박스 css 추가, 구성요소 배열 조정

---
+++ main_func.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $('.box').each(function () {
+        var content = $(this).children('.content');
+        // var content_cut = content_full.split('"'>',2);
+        // var content = content_full.split('>', 2);
+        var content_txt = content.text();
+        var content_txt_short = content_txt.substring(0, 70) + "...";
+        var btn_more = $('<a href="javascript:void(0)" class="more">더보기</a>');
+
+
+        $(this).append(btn_more);
+
+        if (content_txt.length >= 70) {
+            content.html(content_txt_short)
+
+        } else {
+            btn_more.hide()
+        }
+
+        btn_more.click(toggle_content);
+        // 아래 bind가 안 되는 이유는??
+        // btn_more.bind('click',toggle_content);
+
+        function toggle_content() {
+            if ($(this).hasClass('short')) {
+                // 접기 상태
+                $(this).html('더보기');
+                content.html(content_txt_short)
+                $(this).removeClass('short');
+            } else {
+                // 더보기 상태
+                $(this).html('접기');
+                content.html(content_txt);
+                $(this).addClass('short');
+
+            }
+        }
+    });
+});
